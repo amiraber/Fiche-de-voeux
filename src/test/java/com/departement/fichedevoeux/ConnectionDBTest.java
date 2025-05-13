7436446c019bfd378663ff0e9b1d269ea4263d56
amiraber <155098066+amiraber@users.noreply.github.com>
2025-05-13 13:20:44 +0200

    SQLite vers PostgreSQL

--- src/test/java/com/departement/fichedevoeux/ConnectionDBTest.java
+++ src/test/java/com/departement/fichedevoeux/ConnectionDBTest.java
@@ -1,13 +1,15 @@
 package com.departement.fichedevoeux;
-
+// chaque membre doit telecharger postgresql + creer la bdd "fichedb" + modifier son mdp
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionDBTest {
 	public static void main(String[] args) {
-		String url = "jdbc:sqlite:fichedb.db";
-		try (Connection conn = DriverManager.getConnection(url)) {
+		String url = "jdbc:postgresql://localhost:5432/fichedb";
+		String user = "postgres";
+		String password = "amira";  //mettez votre mot de passe hnaya + dans applicaton.properties
+		try (Connection conn = DriverManager.getConnection(url, user, password)) {
 			if (conn != null) {
 				System.out.println("Connexion a la base reussie !");
 			}else {
--- src/test/java/com/departement/fichedevoeux/FichedevoeuxApplicationTests.java
+++ src/test/java/com/departement/fichedevoeux/FichedevoeuxApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FichedevoeuxApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+ @Test
+ void contextLoads() {
+ }
 
 }
