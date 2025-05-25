48f231531530fd52dd5c563ea1f2e037a1edc02d
lynaannou <lynaannou@gmail.com>
2025-05-26 00:54:53 +0100

    cors security fallback

--- src/main/java/com/departement/fichedevoeux/config/WebConfig.java
+++ src/main/java/com/departement/fichedevoeux/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://127.0.0.1:5500") // 👈 ici tu dois avoir la bonne origine
+                .allowedOriginPatterns("*") // 👈 ici tu dois avoir la bonne origine
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
